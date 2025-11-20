88b0f03eab2cf7985229694b1e2fe2907ee79a70
goutamankit96-hub <goutamankit96@gmail.com>
2025-11-20 15:36:52 +0530

    2

---
+++ src/code3.java
@@ -0,0 +1,17 @@
+interface A {
+    abstract void login();
+
+}
+class B implements A{
+    public void login(){
+        System.out.println("hello");
+    }
+}
+class hello{
+    public static void main(String[] args) {
+        B s1=new B();
+        s1.login();
+    }
+}
+
+
